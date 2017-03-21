c963c70f3b8c4a01cc8b7a5b06aad6c9974a623f
Josh Asplund <joshasplund@gmail.com>
2017-03-20 18:53:33 -0700

    Test push

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,33 @@
+use std::ops::Index;
+use std::slice::Iter;
+
+// ID ( Variable | Constant )
+// Predicate (Name, [ID])
+// Rule := Predicate :- [Predicate]
+
+enum ID {
+	Constant(String),
+	Variable(String),
+}
+
+enum RuleID {
+	Constant(String),
+	Variable(String),
+	FreeVariable(String),
+}
+
+struct Predicate<T> {
+	name: String,
+	ids: Vec<T>,
+}
+
+impl <T> Predicate<T> {
+	fn len(&self) -> usize {
+		self.ids.len()
+	}
+
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
