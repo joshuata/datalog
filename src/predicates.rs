134b27b50e4914be5ed8026c2cc1d7842884b8ca
Josh Asplund <joshasplund@gmail.com>
2017-03-22 11:31:35 -0700

    Datalog scaffold built

---
+++ src/predicates.rs
@@ -0,0 +1,106 @@
+use std::fmt::{Display, Formatter};
+use std::fmt::Error;
+
+pub trait Predicate<T>: Display
+    where T: Display
+{
+    fn name(&self) -> &str;
+    fn len(&self) -> usize;
+    fn ids(&self) -> &Vec<T>;
+}
+
+pub struct Pred<'a, T> {
+    name: &'a str,
+    ids: Vec<T>,
+}
+
+pub enum ID<'a> {
+    Literal(&'a str),
+    Variable(&'a str),
+}
+
+pub enum Stmt<'a> {
+    Fact(Pred<'a, &'a str>),
+    Rule {
+        head: Pred<'a, ID<'a>>,
+        tail: Vec<Pred<'a, ID<'a>>>,
+    },
+    Query(Pred<'a, ID<'a>>),
+}
+
+impl<'a> Display for Stmt<'a> {
+    fn fmt(&self, f: &mut Formatter) -> Result<(), Error> {
+        match *self {
+            Stmt::Fact(ref pred) => write!(f, "{}.", pred),
+            Stmt::Rule { ref head, ref tail } => {
+                write!(f, "{} :- ", head).unwrap();
+                tail.iter().fold(true, |first, elem| {
+                    if !first {
+                        write!(f, ", ").unwrap();
+                    }
+                    write!(f, "{}", elem).unwrap();
+                    false
+                });
+                write!(f, ".")
+            }
+            Stmt::Query(ref pred) => write!(f, "{}?", pred),
+        }
+    }
+}
+
+impl<'a, T> Display for Pred<'a, T>
+    where T: Display
+{
+    fn fmt(&self, f: &mut Formatter) -> Result<(), Error> {
+        write!(f, "{}(", self.name).unwrap();
+        for (i, ch) in self.ids.iter().enumerate() {
+            if i == 0 {
+                write!(f, "{}", ch).unwrap();
+            } else {
+                write!(f, ",{}", ch).unwrap();
+            }
+        }
+        write!(f, ")")
+    }
+}
+
+impl<'a> Display for ID<'a> {
+    fn fmt(&self, f: &mut Formatter) -> Result<(), Error> {
+        match *self {
+            ID::Literal(ref x) => write!(f, "'{}'", x),
+            ID::Variable(ref x) => write!(f, "{}", x),
+        }
+    }
+}
+
+impl<'a, T> Predicate<T> for Pred<'a, T>
+    where T: Display
+{
+    fn len(&self) -> usize {
+        self.ids.len()
+    }
+
+    fn name(&self) -> &str {
+        self.name
+    }
+
+    fn ids(&self) -> &Vec<T> {
+        &self.ids
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::{Stmt, ID, Pred};
+
+    #[test]
+    fn it_works() {
+        let test = Stmt::Query(Pred {
+                                   name: "foo",
+                                   ids: vec![ID::Literal("bar"), ID::Literal("baz")],
+                               });
+        let string = format!("{}", test);
+        assert_eq!(string, "foo('bar','baz')?");
+    }
+}
---
+++ src/writer.rs
@@ -0,0 +1,46 @@
+use std::fs::File;
+use std::path::Path;
+use std::io::prelude::*;
+use std::io::Error;
+use predicates::Stmt;
+
+pub trait Writer {
+    fn add(&mut self, Stmt) -> Result<(), Error>;
+}
+
+pub struct FileWriter {
+    file: File,
+}
+
+impl FileWriter {
+    pub fn new(path: &Path) -> FileWriter {
+        let file = File::create(path).unwrap();
+        return FileWriter { file: file };
+    }
+}
+
+impl Writer for FileWriter {
+    fn add(&mut self, statement: Stmt) -> Result<(), Error> {
+        match statement {
+            Stmt::Fact(pred) => writeln!(self.file, "{}.", pred),
+            Stmt::Rule { head, tail } => {
+                write!(self.file, "{} :- ", head).unwrap();
+                tail.iter().fold(true, |first, elem| {
+                    if !first {
+                        write!(self.file, ", ").unwrap();
+                    }
+                    write!(self.file, "{}", elem).unwrap();
+                    false
+                });
+                writeln!(self.file, ".")
+            }
+            Stmt::Query(pred) => writeln!(self.file, "{}?", pred),
+        }
+    }
+}
+
+impl Drop for FileWriter {
+    fn drop(&mut self) {
+        self.file.flush().unwrap();
+    }
+}
